f87eb333b806fd398d7938a4cc421cafa6586add
张邦宸 <zhangbang2002@foxmail.com>
2024-11-29 18:06:09 +0800

    创建搜索静态页面

--- src/components/SearchView.vue
+++ src/components/SearchView.vue
@@ -1,27 +1,41 @@
 <script setup lang="ts">
-import { Search } from '@element-plus/icons-vue'
+import {Search} from '@element-plus/icons-vue'
+import {reactive} from "vue";
+
+const searchObj = reactive({
+  searchInput: ''
+})
+
+function submitSearch(){
+  console.log("开始查询")
+  console.log(searchObj.searchInput)
+}
 </script>
 
 <template>
   <el-container>
-    <el-header><div style="text-align: right;font-size: 15px">辞書モデル</div></el-header>
+    <el-header>
+      <div style="text-align: right;font-size: 15px">辞書モデル</div>
+    </el-header>
     <el-main>
       <el-row justify="center" gutter="20" style="margin-top: 100px">
-        <el-col span="6"> <el-input
-            v-model="input1"
-            style="width: 500px"
-            size="large"
-            placeholder="查询单词"
-            :prefix-icon="Search"/>
+        <el-col span="6">
+          <el-input
+              v-model="searchObj.searchInput"
+              style="width: 500px"
+              size="large"
+              placeholder="查询单词"
+              :prefix-icon="Search"/>
         </el-col>
         <el-col span="6">
-          <el-button size="large" type="primary">查询</el-button>
+          <el-button size="large" type="primary" @click="submitSearch()">查询</el-button>
         </el-col>
       </el-row>
       <el-row justify="center" style="margin-top: 20px">
         <el-col span="6">
           <el-card style="min-width: 587px;min-height: 250px" shadow="hover">
-            <template #header><span>历史记录</span><span style="font-size: 10px;color: #737373">（仅显示最近6条）</span></template>
+            <template #header><span>历史记录</span><span style="font-size: 10px;color: #737373">（仅显示最近6条）</span>
+            </template>
           </el-card>
         </el-col>
       </el-row>
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,7 @@ import 'element-plus/dist/index.css'
 // import './style.css'
 import App from './App.vue'
 import router from "./router";
+import {createPinia} from "pinia";
+const pinia = createPinia()
 
-createApp(App).use(ElementPlus).use(router).mount('#app')
+createApp(App).use(ElementPlus).use(router).use(pinia).mount('#app')
