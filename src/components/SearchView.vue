b23110ea8cd6ef283c7800d533b05b64e9c4c98c
张邦宸 <zhangbang2002@foxmail.com>
2024-12-20 17:00:50 +0800

    创建了Axios，剩余改动在后端

--- src/components/SearchView.vue
+++ src/components/SearchView.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
 import {Search} from '@element-plus/icons-vue'
 import {reactive} from "vue";
+import axios from 'axios';
 
 const searchObj = reactive({
-  searchInput: ''
+  word: ''
 })
 
 function submitSearch(){
   console.log("开始查询")
-  console.log(searchObj.searchInput)
+  console.log(searchObj.word)
+  axios.post(
+      "http://localhost:8080/search",
+      {
+        word : searchObj.word
+      }).then((res:any)=>{
+        console.log(res.status)
+        if(res.status == 200){
+          console.log(res.data.word)
+        }
+  })
 }
 </script>
 
@@ -21,7 +32,7 @@ function submitSearch(){
       <el-row justify="center" gutter="20" style="margin-top: 100px">
         <el-col span="6">
           <el-input
-              v-model="searchObj.searchInput"
+              v-model="searchObj.word"
               style="width: 500px"
               size="large"
               placeholder="查询单词"
